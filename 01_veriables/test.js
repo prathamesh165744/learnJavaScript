d6bb83b3ff7b88e567c35ecaa94dfc3f1d281c69
prathamesh165744 <130149533+prathamesh165744@users.noreply.github.com>
2024-06-18 12:04:37 +0000

    newadd

---
+++ 01_veriables/test.js
@@ -0,0 +1,17 @@
+const studentid= 1888;
+//we con not rewrite this conts or change this const 
+
+let studentEmail="prathamesh";
+ // this is modifiable or later in code we can change this 
+
+
+ var studentname ="prathamesh";
+// it is used in past but now it is not use becase it is one of error of js
+
+
+
+console.log(studentid);
+//this is use for print this veriable that can diffine in it 
+
+console.table([studentid,studentEmail,studentname]);
+//when at a time we print multiple veriable in tabular form 
---
+++ datatypes/demo.js
@@ -0,0 +1,25 @@
+let name='prathamesh';
+// It is sting data type 
+
+//for checking the data type we use 
+console.log(typeof name);
+
+
+let id =1
+
+console.log(typeof id);
+//number 
+
+
+// there are multiple datatypes
+// 1.number
+// 2.string
+// 3.boolean
+// 4.null
+// 5.undifine
+// 6.symbole 
+// 7.bigint
+
+// 8.object
+
+
