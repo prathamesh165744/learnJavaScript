c513d91e59dcf7474a7e2b7bdebb1215cdee0daa
prathamesh165744 <130149533+prathamesh165744@users.noreply.github.com>
2024-07-11 17:41:57 +0000

    learn about arrays methods and object releted methods

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,15 @@
+const Array=['prathamesh','anup','ganu','yash','bhushan']
+
+const result=Array.filter((Array)=>Array.length>5)
+// Array.push(10)
+// Array.pop()
+
+console.log(Array)
+console.log(result)
+Array.unshift('patu')
+console.log(Array)
+Array.shift()
+
+console.log(Array.indexOf('anup'));
+
+console.log(Array)
---
+++ 02_basics/02_object.js
@@ -0,0 +1,12 @@
+const jsuser={
+   fullname:{
+    first:'prathamesh',
+    last:'mapari'
+
+   },
+    age:20,
+    lacation:'washim',
+    email:'prathamesh165744@gmail.com'
+}
+console.log(jsuser.email) //use for retrive simpel object camponats 
+console.log(jsuser.fullname.first)//use for getting nested objects element 
