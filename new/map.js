e24dfc7392bdfb2452f6248db5da73417e49d642
prathamesh165744 <130149533+prathamesh165744@users.noreply.github.com>
2024-07-27 17:59:17 +0000

    new

---
+++ new/map.js
@@ -0,0 +1,8 @@
+const arr=[1,3,4]
+
+const ruptodol=80
+
+const transaction=arr.map(function(n){
+    return n*ruptodol
+})
+console.log(transaction)
---
+++ new/map2doltorup.js
@@ -0,0 +1,10 @@
+const transcation=[100,123,4323,-232]
+
+const doltorup=80
+
+const DollToRup=transcation.map((amount)=>{
+    return(amount/doltorup).toFixed(0)
+})
+console.log(DollToRup)
+
+
